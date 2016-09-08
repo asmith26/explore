44cf477adf6a6c8c7bf0cdc501ef93b61c121201
Adam Wester <awwester@gmail.com>
2016-09-08 18:11:38 -0500

    model basics

---
+++ posts/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import SubReddit, Post, Comment
+
+admin.site.register(SubReddit)
+admin.site.register(Post)
+admin.site.register(Comment)
---
+++ posts/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+
+
+class SubReddit(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    subreddit = models.ForeignKey(SubReddit)
+    create_date = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+
+    def __str__(self):
+        return str(self.subreddit) + ' - ' + self.title
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    post = models.ForeignKey(Post)
+    create_date = models.DateTimeField(auto_now_add=True)
+    body = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return str(self.author.username) + ' - ' + str(self.post.title)
