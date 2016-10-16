c632f76a1e05d8ee11ae69a3c0825b23dc468d28
Adam Wester <awwester@gmail.com>
2016-10-16 14:26:48 -0500

    Routing Basics

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import PostsView
+from .views import PostsView, PostDetailView
 
 
 urlpatterns = [
     url(r'^$', PostsView.as_view()),
+    url(r'^(?P<pk>\d+)/$', PostDetailView.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,14 @@
 from django.views.generic.list import ListView
+from django.views.generic import DetailView
 
 from .models import Post
 
 
 class PostsView(ListView):
     template_name = "posts/posts.html"
+    model = Post
+
+
+class PostDetailView(DetailView):
+    template_name = "posts/post.html"
     model = Post
