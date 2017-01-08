e2f47f7dc1125b0b78f9447c88b6bb8b8b4c5043
Adam Wester <awwester@gmail.com>
2017-01-08 06:10:51 -0500

    send contact email

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
-from .views import HomeView, AboutView
+from .views import HomeView, AboutView, ContactView, ContactSendView
 
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name="home"),
     url(r'^about/$', AboutView.as_view(), name="about"),
+    url(r'^contact/$', ContactView.as_view(), name="contact"),
+    url(r'^contact/send/$', ContactSendView.as_view(), name="contact-send"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.core.mail import send_mail
+
+from braces import views
 
 from .mixins import PageMixin
 
@@ -11,3 +14,29 @@ class HomeView(PageMixin, TemplateView):
 class AboutView(PageMixin, TemplateView):
     template_name = "about.html"
     page = "about"
+
+
+class ContactView(PageMixin, TemplateView):
+    template_name = "contact.html"
+    page = "contact"
+
+class ContactSendView(views.CsrfExemptMixin, views.JsonRequestResponseMixin, View):
+    require_json = True
+
+    def post(self, request, *args, **kwargs):
+        try:
+            name = self.request_json["email"]
+            name = self.request_json["name"]
+            subject = self.request_json["subject"]
+            message = self.request_json["message"]
+
+            if not len(name) or not len(subject) or not len(message):
+                raise KeyError
+        except KeyError:
+            error_dict = {"message": "you must include all fields"}
+            return self.render_bad_request_response(error_dict)
+
+        send_mail(subject, message, "myemail@mydomain.com", ["you@yourdomain.com"])
+
+        return self.render_json_response(
+            {"message": "Your contact has been sent!"})
